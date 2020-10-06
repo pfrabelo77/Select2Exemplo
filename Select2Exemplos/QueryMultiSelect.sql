d1d41fc3e70c8159ae1a030cb32e98aef915c6be
Paulo Fernando Rabelo <paulo.rabelo@wayon.global>
2020-10-06 10:18:24 -0300

    Nova function SplitString SQL

--- Select2Exemplos/QueryMultiSelect.sql
+++ Select2Exemplos/QueryMultiSelect.sql
@@ -1,7 +1,9 @@
 DECLARE @List VARCHAR(max) = '2,3,5' --Coloque aqui os valores dos IDs vindos do Select2 MultiSelect
 
+--SELECT item FROM dbo.SplitString(@list,',')
+
 SELECT *
 FROM   Clientes
-WHERE  (Id IN (SELECT item FROM dbo.SplitStrings_CTE(@list,',')) OR @List IS NULL) --Utilize esta function no where para buscar os IDs escolhido no Select2 MultiSelect
+WHERE  (Id IN (SELECT item FROM dbo.SplitString(@list,',')) OR @List IS NULL) --Utilize esta function no where para buscar os IDs escolhido no Select2 MultiSelect
 
 
