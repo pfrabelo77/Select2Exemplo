663b039a3b1712daaafa4116e199735c0d6ec36e
Paulo Fernando Rabelo <paulo.rabelo@wayon.global>
2020-10-08 15:05:57 -0300

    Query criar tbCarro

---
+++ Select2Exemplos/QueryCriarTabelaCarro.sql
@@ -0,0 +1,50 @@
+create table tbCarro
+(
+	id int identity(1,1) primary key,
+	Nome varchar(100),
+)
+
+Go
+
+insert into tbCarro (Nome) values ('Brasilia')
+insert into tbCarro (Nome) values ('Gol')
+insert into tbCarro (Nome) values ('Fiat 147')
+insert into tbCarro (Nome) values ('Passat')
+insert into tbCarro (Nome) values ('Parati')
+insert into tbCarro (Nome) values ('Monza')
+insert into tbCarro (Nome) values ('Voyage')
+insert into tbCarro (Nome) values ('Opala')
+insert into tbCarro (Nome) values ('Caravan')
+insert into tbCarro (Nome) values ('Fusca')
+insert into tbCarro (Nome) values ('Fiorino')
+insert into tbCarro (Nome) values ('Pampa')
+insert into tbCarro (Nome) values ('Chevete')
+insert into tbCarro (Nome) values ('Belina')
+insert into tbCarro (Nome) values ('Del Rey')
+insert into tbCarro (Nome) values ('Lada')
+insert into tbCarro (Nome) values ('Niva')
+insert into tbCarro (Nome) values ('Landau')
+insert into tbCarro (Nome) values ('Palio')
+insert into tbCarro (Nome) values ('Tempra')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Select2Exemplos/QueryMultiSelect.sql
+++ Select2Exemplos/QueryMultiSelect.sql
@@ -3,7 +3,7 @@ DECLARE @List VARCHAR(max) = '2,3,5' --Coloque aqui os valores dos IDs vindos do
 --SELECT item FROM dbo.SplitString(@list,',')
 
 SELECT *
-FROM   Clientes
+FROM   tbCarro
 WHERE  (Id IN (SELECT item FROM dbo.SplitString(@list,',')) OR @List IS NULL) --Utilize esta function no where para buscar os IDs escolhido no Select2 MultiSelect
 
 
